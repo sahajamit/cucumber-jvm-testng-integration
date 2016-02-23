7d2e1684018262cadceae325aca8f0a17fc7e993
Amit Rawat <sahajamit@gmail.com>
2016-02-23 17:08:36 +0800

    updated runcukestest

--- src/test/java/com/cucumber/testng/examples/RunCukesTest.java
+++ src/test/java/com/cucumber/testng/examples/RunCukesTest.java
@@ -5,10 +5,9 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 /**
  * Created by Amit Rawat on 2/23/2016.
  */
-
-@CucumberOptions(features = "src/test/resources/com.cucumber.testng.examples/date_calculator2.feature", format = { "pretty",
-        "html:target/site/cucumber-pretty2",
-        "json:target/cucumber2.json" })
+@CucumberOptions(features = "src/test/resources/com.cucumber.testng.examples/date_calculator1.feature", format = { "pretty",
+        "html:target/site/cucumber-pretty",
+        "json:target/cucumber1.json" })
 public class RunCukesTest extends AbstractTestNGCucumberTests {
 }
 
