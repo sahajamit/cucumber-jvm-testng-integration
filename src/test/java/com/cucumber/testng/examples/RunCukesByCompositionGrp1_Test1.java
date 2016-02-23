2f86bd29cf00ccd75537c9e72f18483a6f07a059
Amit Rawat <sahajamit@gmail.com>
2016-02-23 16:21:02 +0800

    more refactoring

--- src/test/java/com/cucumber/testng/examples/RunCukesByCompositionGrp1_Test1.java
+++ src/test/java/com/cucumber/testng/examples/RunCukesByCompositionGrp1_Test1.java
@@ -11,9 +11,9 @@ import org.testng.annotations.Test;
  * An example of using TestNG when the test class does not inherit from
  * AbstractTestNGCucumberTests.
  */
-//@CucumberOptions(features = "src/test/resources/com.cucumber.testng.examples/date_calculator1.feature", format = { "pretty",
-//        "html:target/site/cucumber-pretty2",
-//        "json:target/cucumber1.json" })
+/**
+ * Created by amit.rawat on 06/01/16.
+ */
 @CucumberOptions(features = "src/test/resources/com.cucumber.testng.examples/date_calculator1.feature", plugin = "json:target/cucumber1.json")
 public class RunCukesByCompositionGrp1_Test1 {
     @BeforeSuite
