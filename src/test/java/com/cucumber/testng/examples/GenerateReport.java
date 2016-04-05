14efc0992dabdb916d1800d75cb26e90d07ffd51
Amit Rawat <sahajamit@gmail.com>
2016-04-05 10:02:32 +0800

    cleaned up some code from reporting class file

--- src/test/java/com/cucumber/testng/examples/GenerateReport.java
+++ src/test/java/com/cucumber/testng/examples/GenerateReport.java
@@ -35,43 +35,6 @@ public class GenerateReport {
                     buildProject, skippedFails, pendingFails, undefinedFails, missingFails, flashCharts, runWithJenkins,
                     highCharts, parallelTesting);
 
-//            ReportBuilder reportBuilder = new ReportBuilder(list, reportOutputDirectory, pluginUrlPath, buildNumber,
-//                    buildProject, skippedFails, undefinedFails, flashCharts, runWithJenkins, artifactsEnabled, artifactConfig,
-//                    highCharts);
-            reportBuilder.generateReports();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-    public static void main(String args[]){
-        try{
-            String RootDir = System.getProperty("user.dir");
-            File reportOutputDirectory = new File("target/Masterthought");
-            List<String> list = new ArrayList<String>();
-            list.add("target/cucumber1.json");
-            list.add("target/cucumber2.json");
-
-            String pluginUrlPath = "";
-            String buildNumber = "1";
-            String buildProject = "cucumber-jvm";
-            boolean skippedFails = true;
-            boolean pendingFails = true;
-            boolean undefinedFails = true;
-            boolean missingFails = true;
-            boolean flashCharts = true;
-            boolean runWithJenkins = false;
-            boolean highCharts = false;
-            boolean parallelTesting = true;
-            boolean artifactsEnabled = false;
-            String artifactConfig = "";
-
-            ReportBuilder reportBuilder = new ReportBuilder(list, reportOutputDirectory, pluginUrlPath, buildNumber,
-                    buildProject, skippedFails, pendingFails, undefinedFails, missingFails, flashCharts, runWithJenkins,
-                    highCharts, parallelTesting);
-
-//            ReportBuilder reportBuilder = new ReportBuilder(list, reportOutputDirectory, pluginUrlPath, buildNumber,
-//                    buildProject, skippedFails, undefinedFails, flashCharts, runWithJenkins, artifactsEnabled, artifactConfig,
-//                    highCharts);
             reportBuilder.generateReports();
         }catch(Exception e){
             e.printStackTrace();
